fdeea20559d85d1135777059ebd29a811a047099
guyuqiang <guyuqiang@huishoubao.com.cn>
2019-08-08 12:26:55 +0800

    init

---
+++ app/src/main/java/com/example/pc/kotlin/TestJava.java
@@ -0,0 +1,13 @@
+package com.example.pc.kotlin;
+
+/**
+ * ${description}
+ *
+ * @author gyq
+ * @date 2019/8/7
+ */
+public class TestJava {
+    public static void main(String[] args) {
+        System.out.print(1);
+    }
+}
