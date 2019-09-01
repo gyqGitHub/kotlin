01b35cef747f26f97c05376a2b511c6b095c90c1
guyuqiang <1292351633@qq.com>
2019-09-02 00:22:58 +0800

    update

--- app/src/test/java/com/example/pc/kotlin/coroutine/Basics.kt
+++ app/src/test/java/com/example/pc/kotlin/coroutine/Basics.kt
@@ -3,6 +3,8 @@ package com.example.pc.kotlin.coroutine
 import kotlinx.coroutines.*
 import org.junit.Test
 import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.CoroutineContext.Element
+import kotlin.coroutines.CoroutineContext.Key
 
 /**
  * 协程基础
@@ -12,17 +14,24 @@ class Basics {
     /**
      * 本质上，协程是轻量级的线程
      * 协程的创建方式：
-     * 1.协程作用域+协程构建器一起创建，GlobalScope.launch{},GlobalScope是协程作用域，launch{}是协程构建器
+     * 1.协程作用域+协程构建器一起创建，GlobalScope是协程作用域，GlobalScope.launch{}是协程构建器
      * --1.协程作用域：属性+方法[CoroutineScope]
-     * ------1.属性：协程上下文[CoroutineContext]
+     * ------1.属性：协程上下文[CoroutineContext]，除了访问高级用法的Job实例外，不建议访问此属性
+     * -----------1·一个接口，是[Element]实例的索引集合，每个[Element]在此集合都有唯一的[Key]，一个[Element]本身就是一个协程上下文单例
      * ------2.方法：只有一系列的扩展方法(协程构建器)
-     * ------3.获取独立作用域实例的最好方法是[CoroutineScope()] and [MainScope()]工厂方法???
+     *
+     * ------3.获取独立作用域实例的最好方法是[CoroutineScope()] and [MainScope()]工厂方法，不建议手动实现CoroutineScope接口???
+     * ------4.每个协程构建器和作用域函数都提供自己的作用域和Job（继承了[Element]）进入它运行的内部代码块，都会等待子协程的完成，因此能够强制执行结构化并发原则
+     * ------5·协程作用域应在具有明确定义生命周期的实体上实现（或用作字段），有这些实体负责启动子协程
      *
      * --2.协程构建器：
-     * ------1.每个协程构建器都是协程作用域的扩展[CoroutineScope.launch]
-     * ------2.构建器扩展方法：[CoroutineScope.launch],[CoroutineScope.async]
+     * ------1.每个协程构建器都是协程作用域的扩展方法,并且会继承作用域上下文以自动传播上下文元素和取消异常
+     * ------2.扩展方法：[CoroutineScope.launch],[CoroutineScope.async]
      *
-     * --3.协程作用域派生类[GlobalScope]
+     * --3.协程作用域派生类
+     * ------1·[GlobalScope] 全局作用域不受任何Job的约束
+     *
+     * ------2·[MainScope] 用于ui组件中
      *
      * 2.[runBlocking]创建一个会阻塞当前线程的协程
      */
@@ -195,5 +204,102 @@ class Basics {
             println("job2.isCancelled = ${job2.isCancelled}")
         }
         job.join() // 等待直到子协程执行结束
+    }
+
+    /**
+     * 作用域构建器
+     * 1）coroutineScope创建一个协程作用域，该协程作用域的协程上下文继承外部作用域，但[Job]会被覆盖掉
+     * 2）此作用域用于并行和分解工作，该作用域内的任何一个子协程失败都会导致其他子协程取消和作用域内代码执行失败
+     * 3）使用示例
+     * suspend fun showSomeData() = coroutineScope {
+     *
+     *   val data = async(Dispatchers.IO) { // <- extension on current scope
+     *      ... load some UI data for the Main thread ...
+     *   }
+     *
+     *   withContext(Dispatchers.Main) {
+     *     doSomeWork()
+     *     val result = data.await()
+     *     display(result)
+     *   }
+     * }
+     * ---1·如果doSomeWork()抛出了异常A，那么async也会被取消，showSomeData()继续抛出异常A
+     * 4）如果当前的Job被外部取消，showSomeData()因为是挂起函数可能会抛出[CancellationException]；如果该作用域内出现了未处理的异常，还会抛出相应的异常
+     */
+    @Test
+    fun main6() = runBlocking { // this: CoroutineScope
+        val job1 = launch {
+            delay(200L)
+            println("Task from runBlocking")
+        }
+//        job1.join()//取消注释后显式阻塞下面代码的执行
+        coroutineScope { // Creates a coroutine scope//会挂起协程的执行，阻塞后面的代码
+            launch {
+                delay(500L)
+                println("Task from nested launch")
+            }
+
+            delay(100L)
+            println("Task from coroutine scope") // This line will be printed before the nested launch
+        }
+
+        println("Coroutine scope is over") // This line is not printed until the nested launch completes
+    }
+
+    @Test
+    fun main7() = runBlocking { // this: CoroutineScope
+        val job1 = launch {
+            try {
+                coroutineScope {
+                    // Creates a coroutine scope//会挂起协程的执行，阻塞后面的代码
+                    launch {
+                        delay(2000L)
+                        println("Task from nested launch")
+                    }
+
+                    println("验证外部Job调用了cancel()后该作用域将抛出取消异常") // This line will be printed before the nested launch
+
+                    try {
+                        launch {
+                            delay(30000)
+                            println("我还能打印，证明我没被取消！！！")
+                        }
+                    } catch (e: CancellationException) {
+                        println("能收到取消异常吗！！！")//不会捕捉到取消异常
+                    }
+                }
+            } catch (e: CancellationException) {
+                println(e.message)
+            }
+
+        }
+        delay(200)
+        job1.cancel(CancellationException("该作用域外部Job调用了cancel()"))
+
+        val job2 = launch {
+            try {
+                coroutineScope {
+                    // Creates a coroutine scope//会挂起协程的执行，阻塞后面的代码
+                    launch {
+                        delay(2000L)
+                        throw ArithmeticException("子协程抛出异常了")
+                        println("Task from nested launch")
+                    }
+                    println("验证该作用域内某个子协程中抛出了异，则该作用域继续抛出相应的异常")
+
+                    try {
+                        launch {
+                            delay(30000)
+                            println("我还能打印，证明我没被取消！！！")
+                        }
+                    } catch (e: CancellationException) {
+                        println("能收到取消异常吗！！！")//不会捕捉到取消异常
+                    }
+                }
+            } catch (e: ArithmeticException) {
+                println(e.message)
+            }
+
+        }
     }
 }
